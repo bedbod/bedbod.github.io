0af908758870f8c5c0644208c22bc3b73ce98dde
piss-catalogue <boerbenny9@gmail.com>
2026-08-12 22:38:58 +0200

    about and contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,37 @@ function filterSelection(className) {
                     div.style.display = "block";
                 }
             }
-            var divs = document.getElementsByClassName("btn");
+            var divs = document.getElementsByClassName("gallerybtn");
             for (var i = 0; i < divs.length; i++) {
                 var div = divs[i];
                 if (div.classList.contains(className)) {
                     div.classList.add("active");
+                } else {
+                    div.classList.remove("active")
+                }
+            }
+        }
+
+ShowNav()
+function ShowNav(className) {
+            var divs = document.getElementsByClassName("nav");
+            for (var i = 0; i < divs.length; i++) {
+                var div = divs[i];
+                if (div.classList.contains("active")) {
+                    div.classList.remove("active")
+                } else if (div.classList.contains(className)) {
+                    div.classList.add("active")
+                } else {
+                    div.classList.remove("active")
+                }
+            }
+            var divs = document.getElementsByClassName("navbtn");
+            for (var i = 0; i < divs.length; i++) {
+                var div = divs[i];
+                if (div.classList.contains("active")) {
+                    div.classList.remove("active")
+                } else if (div.classList.contains(className)) {
+                    div.classList.add("active")
                 } else {
                     div.classList.remove("active")
                 }
